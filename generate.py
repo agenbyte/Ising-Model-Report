f315340115b899ade8e1aecd66ed24fe12c901b1
John Kaushagen <johnkaushagen@gmail.com>
2018-05-06 17:48:22 -0400

    grr

--- generate.py
+++ generate.py
@@ -1,12 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 from ising import Simulation
 
-temps = range(11)
 
-e = []
-for t in temps:
-    print(t)
-    a = Simulation(32, t)
-    a.step_n_times(1000000)
-    e.append(a.get_mean())
+a = Simulation(16, 1000)
+a.step_n_times(100000)
 
-print(e)
+plt.plot(np.array(a.energy_history)/(a.config.shape[0]**2))
+plt.show()
+plt.pcolor(a.config)
+plt.show()
+
--- ising.py
+++ ising.py
@@ -1,90 +1,61 @@
-'''
-Contains code for simulating the Ising model
-on a square grid.
-'''
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-class Simulation(object):
-    '''
-    The Simulation object stores data about the running simulation.
-    It also allows us to do some cool things.
-    If you give it your own configuration, it must have periodic
-    boundary conditions already
-    '''
-    def __init__(self, size, temperature, config=None):
-        if config is None:
-            self.config = self.generate_random_lattice(size)
+SIZE = 32
+NITERATIONS = 50 * SIZE**2
+TEMPERATURE = 5.26919
+NTEMPS = 11
+
+
+TEMPERATURE_ARRAY = np.linspace(0.1, 2, NTEMPS)
+ENERGY_ARRAY = np.zeros(NTEMPS)
+MAGN_ARRAY = np.zeros(NTEMPS)
+MAGS_ARRAY = np.zeros(NTEMPS)
+BASE_LAT = 2*np.random.randint(2, size=(SIZE+2, SIZE+2))-1
+
+for ii, T in enumerate(TEMPERATURE_ARRAY):
+    # Set up a new lattice
+    print(ii)
+    lat = BASE_LAT
+    ehist = np.zeros(NITERATIONS+1)
+    mhist = np.zeros(NITERATIONS+1)
+    mshist = np.zeros(NITERATIONS)
+    shhist = np.zeros(NITERATIONS)
+
+    # Now set the periodic boundary conditions
+    lat[ 0,  1:-1] = lat[-2,  1:-1]
+    lat[-1,  1:-1] = lat[ 1,  1:-1]
+    lat[ 1:-1,  0] = lat[ 1:-1, -2]
+    lat[ 1:-1, -1] = lat[ 1:-1,  1]
+    lat[0, 0] = lat[-1, 0] = lat[0, -1] = lat[-1, -1] = 0
+
+    energy = 0
+    for i in range(1, SIZE+1):
+        for j in range(1, SIZE+1):
+            neighbors = lat[i+1, j] + lat[i-1, j] + lat[i,j+1] + lat[i,j-1]
+            energy += lat[i, j] * neighbors
+    energy /= -2.0
+
+    ehist[0] = energy
+
+    for step in range(NITERATIONS):
+        si, sj = np.random.randint(1, SIZE+1, size=2)
+        delta_E = 2 * lat[si, sj] * (lat[si+1,sj] + lat[si-1,sj] + lat[si,sj+1] + lat[si,sj-1])
+        p = min(1, np.exp(-delta_E/T))
+        if np.random.uniform() < p:
+            lat[si, sj] *= -1
+            ehist[step+1] = ehist[step] + delta_E
+            mhist[step+1] = abs(np.sum(lat)/SIZE**2)
         else:
-            self.config = config
-        self.size = size
-        self.temperature = temperature
-        self.energy_history = []
-        self.energy = self.calculate_energy()
-        self.energy_history.append(self.energy)
+            ehist[step+1] = ehist[step] + delta_E
+            mhist[step+1] = abs(np.sum(lat)/SIZE**2)
+    plt.plot(ehist)
+    plt.show()
+    e = ehist[1000:].mean()
+    m = mhist[1000:].mean()
+    ENERGY_ARRAY[ii] = e
+    MAGN_ARRAY[ii] = m
 
-    def step(self):
-        lattice_site = self.choose_random_lattice_site()
-        energy_change = self.compute_energy_change(lattice_site)
-        acceptance_probability = min(1, np.exp(self.temperature*energy_change))
-        if np.random.uniform() < acceptance_probability:
-            self.flip_lattice_site(lattice_site)
-            self.energy += energy_change
-            self.config = set_period_boundary(self.config)
-
-    def step_n_times(self, nsteps):
-        for __ in range(nsteps):
-            self.step()
-            self.energy_history.append(self.energy)
-
-    def calculate_energy(self):
-        '''
-        Computes the energy of the system
-        '''
-        energy = 0
-        for i in range(1, self.size-1):
-            for j in range(1, self.size-1):
-                energy += self.config[i-1, j] * self.config[i, j] +\
-                          self.config[i+1, j] * self.config[i, j] +\
-                          self.config[i, j-1] * self.config[i, j] +\
-                          self.config[i, j+1] * self.config[i, j]
-        return energy/4.0
-
-    def generate_random_lattice(self, size):
-        '''
-        Generates a 2-dimensional lattice of -1's and 1's
-        and applies periodic boundary conditions to the system
-        '''
-        size = size + 2
-        config = 2*np.random.randint(0, high=2, size=(size, size)) - 1
-        config = set_period_boundary(config)
-        return config
-
-    def flip_lattice_site(self, lattice_site):
-        self.config[lattice_site] *= -1
-
-    def compute_energy_change(self, lattice_site):
-        print(self.config[lattice_site])
-        print(lattice_site)
-        left = lattice_site - (1, 0)
-        right = lattice_site + (1, 0)
-        up = lattice_site + (0, 1)
-        down = lattice_site - (0, 1)
-        delta_energy = -2 * self.config[lattice_site] *\
-                            (self.config[left] + self.config[right] +\
-                             self.config[up] + self.config[down])
-        print(delta_energy)
-        return delta_energy
-
-    def choose_random_lattice_site(self):
-        flip_site = np.random.randint(1, high=self.size+1, size=(2))
-        return flip_site
-
-
-
-def set_period_boundary(config):
-    config[:, -1] = config[:, 1]
-    config[:, 0] = config[:, -2]
-    config[-1, :] = config[1, :]
-    config[0, :] = config[-2, :]
-    return config
+plt.plot(ENERGY_ARRAY)
+plt.show()
