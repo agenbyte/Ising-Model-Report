f53b43034151b448a08a79ebe74eb152a72b0d52
John Kaushagen <johnkaushagen@gmail.com>
2018-05-08 17:52:05 -0400

    clean

--- ising.py
+++ ising.py
@@ -1,72 +1,69 @@
+# Main functions for Ising simulation
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-SIZE = 2**4
-NITERATIONS = 2**10*SIZE
-NTEMPS = 2**8
+def ising_step(lat, size, temperature):
+    '''
+    Does one Metropolis step for the simulation.
+    Returns the resulting lattice and the energy change.
+    '''
+    si, sj = np.random.randint(1, size+1, size=2)
+    neighbors = np.array(lat[si,sj]!=np.array(
+            [lat[si+1, sj], lat[si-1, sj], lat[si,sj+1], lat[si,sj-1]]
+        ), dtype=int)
+    delta_E = 4 - 2*sum(neighbors)
+    p = min(1, np.exp(-delta_E/temperature))
+    if np.random.uniform() < p:
+        lat[si, sj] *= -1
+        lat[ 0,  1:-1] = lat[-2,  1:-1]
+        lat[-1,  1:-1] = lat[ 1,  1:-1]
+        lat[ 1:-1,  0] = lat[ 1:-1, -2]
+        lat[ 1:-1, -1] = lat[ 1:-1,  1]
+        lat[0, 0] = lat[-1, 0] = lat[0, -1] = lat[-1, -1] = 0
+    else:
+        delta_E = 0
+    return lat, delta_E
 
 
-TEMPERATURE_ARRAY = np.random.normal(2.26919, .64, NTEMPS)
-ENERGY_ARRAY = np.zeros(NTEMPS)
-MAGN_ARRAY = np.zeros(NTEMPS)
-MAGS_ARRAY = np.zeros(NTEMPS)
+def ising_simulation(size, temperature, niterations, lat):
+    '''
+    Does N Metropolis steps of the ising model. Returns
+    energy and magnetization histories.
+    '''
+    ehist = np.zeros(niterations+1)
+    mhist = np.zeros(niterations+1)
+    e0 = calc_energy(lat, size)
+    ehist[0] = e0
+    for step in range(niterations):
+        lat, delta_E = ising_step(lat, size, temperature)
+        ehist[step+1] = ehist[step] + delta_E
+        mhist[step+1] = abs(np.sum(lat[1:-1,1:-1])/size**2)
+    return ehist, mhist
 
-for ii, T in enumerate(TEMPERATURE_ARRAY):
-    lat = 2*np.random.randint(2, size=(SIZE+2, SIZE+2))-1
-    ehist = np.zeros(NITERATIONS+1)
-    mhist = np.zeros(NITERATIONS+1)
 
-
-    lat[ 0,  1:-1] = lat[-2,  1:-1]
-    lat[-1,  1:-1] = lat[ 1,  1:-1]
-    lat[ 1:-1,  0] = lat[ 1:-1, -2]
-    lat[ 1:-1, -1] = lat[ 1:-1,  1]
-    lat[0, 0] = lat[-1, 0] = lat[0, -1] = lat[-1, -1] = 0
-
+def calc_energy(lat, size):
+    '''
+    Calculates the energy of the given lattice
+    '''
     energy = 0
-    for i in range(1, SIZE+1):
-        for j in range(1, SIZE+1):
+    for i in range(1, size+1):
+        for j in range(1, size+1):
             neighbors = np.array(lat[i,j]!=np.array(
                 [lat[i+1, j], lat[i-1, j], lat[i,j+1], lat[i,j-1]]
             ), dtype=int)
             energy += sum(neighbors)
     energy /= 2.0
+    return energy
 
-    ehist[0] = energy
-    mhist[0] = abs(np.sum(lat))/SIZE**2
 
-    for step in range(NITERATIONS):
-
-        si, sj = np.random.randint(1, SIZE+1, size=2)
-        neighbors = np.array(lat[si,sj]!=np.array(
-                [lat[si+1, sj], lat[si-1, sj], lat[si,sj+1], lat[si,sj-1]]
-            ), dtype=int)
-        delta_E = 4 - 2 * sum(neighbors)
-        p = min(1, np.exp(-delta_E/T))
-        if np.random.uniform() < p:
-            lat[si, sj] *= -1
-            ehist[step+1] = ehist[step] + delta_E
-            mhist[step+1] = abs(np.sum(lat[1:-1,1:-1])/SIZE**2)
-            lat[ 0,  1:-1] = lat[-2,  1:-1]
-            lat[-1,  1:-1] = lat[ 1,  1:-1]
-            lat[ 1:-1,  0] = lat[ 1:-1, -2]
-            lat[ 1:-1, -1] = lat[ 1:-1,  1]
-            lat[0, 0] = lat[-1, 0] = lat[0, -1] = lat[-1, -1] = 0
-        else:
-            ehist[step+1] = ehist[step]
-            mhist[step+1] = abs(np.sum(lat[1:-1,1:-1])/SIZE**2)
-
-    ehist /= SIZE**2
-    # plt.plot(ehist)
-    # plt.show()
-    # plt.pcolor(lat)
-    # plt.show()
-    e = ehist[2**9:].mean()
-    m = mhist[2**9:].mean()
-    ENERGY_ARRAY[ii] = e
-    MAGN_ARRAY[ii] = m
-
-plt.scatter(TEMPERATURE_ARRAY, ENERGY_ARRAY)
-plt.show()
-
+def compute_ACcF(data, start, end, steps):
+    '''
+    Computes the autocorrelation of the data.
+    '''
+    ACvF = []
+    for i in range(steps):
+        cov = np.corrcoef(data[start:end], data[start+i:end+i])[0][1]
+        ACvF.append(cov)
+    return ACvF
--- make_figs.py
+++ make_figs.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-import ising_sw
+import ising
 
 #np.random.seed(seed=0) # Reproducibility
 
@@ -18,7 +18,7 @@ def make_warmup_time_figure():
     lat[ 1:-1,  0] = lat[ 1:-1, -2]
     lat[ 1:-1, -1] = lat[ 1:-1,  1]
     lat[0, 0] = lat[-1, 0] = lat[0, -1] = lat[-1, -1] = 0
-    e, __ = ising_sw.ising_simulation(size, temperature, nsteps, lat)
+    e, __ = ising.ising_simulation(size, temperature, nsteps, lat)
     e /= size**2
     plt.figure(figsize=(5,4))
     plt.plot(e)
@@ -30,89 +30,92 @@ def make_warmup_time_figure():
 
 def make_pcolor_figures():
     size = 100
-    tc = 1.13459265711
-    t1 = 0.5 * tc
-    t2 = 1.5 * tc
-    base = 2 * np.random.randint(0, 2, size=(size+2, size+2)) - 1
-    # WHY DOESN'T THIS WORK OMGGGGGGGGG
+    tc = 1.1346
+    t0 = 0.25 * tc
+    t1 = tc
+    t2 = 2 * tc
+    base_lattice = 2 * np.random.randint(0, 2, size=(size+2, size+2)) - 1
+    base_lattice[ 0,  1:-1] = base_lattice[-2,  1:-1]
+    base_lattice[-1,  1:-1] = base_lattice[ 1,  1:-1]
+    base_lattice[ 1:-1,  0] = base_lattice[ 1:-1, -2]
+    base_lattice[ 1:-1, -1] = base_lattice[ 1:-1,  1]
+    base_lattice[0, 0] = base_lattice[-1, 0] = base_lattice[0, -1] = base_lattice[-1, -1] = 0
 
-    lat = np.copy(base)
-    # Set the periodic bc's
-    lat[ 0,  1:-1] = lat[-2,  1:-1]
-    lat[-1,  1:-1] = lat[ 1,  1:-1]
-    lat[ 1:-1,  0] = lat[ 1:-1, -2]
-    lat[ 1:-1, -1] = lat[ 1:-1,  1]
-    lat[0, 0] = lat[-1, 0] = lat[0, -1] = lat[-1, -1] = 0
-    plt.figure(figsize=(4,4))
-    plt.pcolor(lat[1:-1, 1:-1])
-    plt.xlabel('Does this work?')
-    plt.savefig(os.path.join('Figures/Step_1_Tc.eps'))
+    lat = np.copy(base_lattice)
+
+    f, ((ax1, ax2, ax3), (ax4, ax5, ax6), (ax7, ax8, ax9)) = plt.subplots(3, 3, sharex='col', sharey='row')
+    ax1.pcolor(lat[1:-1, 1:-1])
+    ax1.set_title('t=1')
+    ax2.set_title('t=10000')
+    ax3.set_title('t=100000')
+    ax1.set_ylabel(r'$T=0.25T_c$')
+    ax4.set_ylabel(r'$T=T_c$')
+    ax7.set_ylabel(r'$T=2T_c$')
 
     for __ in range(10000):
         print('simming')
-        lat, __ = ising_sw.ising_step(lat, size, tc)
-    #plt.figure(figsize=(4,4))
-    plt.pcolor(lat[1:-1, 1:-1])
-    plt.xlabel('Does this work?')
-    plt.savefig(os.path.join('Figures/Step_10000_Tc.eps'))
+        lat, __ = ising.ising_step(lat, size, t0)
+    ax2.pcolor(lat[1:-1, 1:-1])
 
     for __ in range(90000):
         print('simming')
-        lat, __ = ising_sw.ising_step(lat, size, tc)
-    #plt.figure(figsize=(4,4))
-    plt.pcolor(lat[1:-1, 1:-1])
-    plt.xlabel('Does this work?')
-    plt.savefig(os.path.join('Figures/Step_100000_Tc.eps'))
+        lat, __ = ising.ising_step(lat, size, tc)
+    ax3.pcolor(lat[1:-1, 1:-1])
 
-    lat = np.copy(base)
-    lat[ 0,  1:-1] = lat[-2,  1:-1]
-    lat[-1,  1:-1] = lat[ 1,  1:-1]
-    lat[ 1:-1,  0] = lat[ 1:-1, -2]
-    lat[ 1:-1, -1] = lat[ 1:-1,  1]
-    lat[0, 0] = lat[-1, 0] = lat[0, -1] = lat[-1, -1] = 0
-    #plt.figure(figsize=(4,4))
-    plt.pcolor(lat[1:-1, 1:-1])
-    plt.xlabel('Does this work?')
-    plt.savefig(os.path.join('Figures/Step_1_0.5Tc.eps'))
+    lat = np.copy(base_lattice)
+    ax4.pcolor(lat[1:-1, 1:-1])
+
     for __ in range(10000):
-        print('simming')
-        lat, __ = ising_sw.ising_step(lat, size, t1)
-    #plt.figure(figsize=(4,4))
-    plt.pcolor(lat[1:-1, 1:-1])
-    plt.xlabel('Does this work?')
-    plt.savefig(os.path.join('Figures/Step_10000_0.5Tc.eps'))
-    for __ in range(90000):
-        print('simming')
-        lat, __ = ising_sw.ising_step(lat, size, t1)
-    #plt.figure(figsize=(4,4))
-    plt.pcolor(lat[1:-1, 1:-1])
-    plt.xlabel('Does this work?')
-    plt.savefig(os.path.join('Figures/Step_100000_0.5Tc.eps'))
+        lat, __ = ising.ising_step(lat, size, t1)
+    ax5.pcolor(lat[1:-1, 1:-1])
 
-    lat = np.copy(base)
-    lat[ 0,  1:-1] = lat[-2,  1:-1]
-    lat[-1,  1:-1] = lat[ 1,  1:-1]
-    lat[ 1:-1,  0] = lat[ 1:-1, -2]
-    lat[ 1:-1, -1] = lat[ 1:-1,  1]
-    lat[0, 0] = lat[-1, 0] = lat[0, -1] = lat[-1, -1] = 0
-    #plt.figure(figsize=(4,4))
-    plt.pcolor(lat[1:-1, 1:-1])
-    plt.xlabel('Does this work?')
-    plt.savefig(os.path.join('Figures/Step_1_1.5Tc.eps'))
-    for __ in range(10000):
-        print('simming')
-        lat, __ = ising_sw.ising_step(lat, size, t2)
-    plt.figure(figsize=(4,4))
-    plt.pcolor(lat[1:-1, 1:-1])
-    plt.xlabel('Does this work?')
-    plt.savefig(os.path.join('Figures/Step_10000_1.5Tc.eps'))
     for __ in range(90000):
-        print('simming')
-        lat, __ = ising_sw.ising_step(lat, size, t2)
-    plt.figure(figsize=(4,4))
-    plt.pcolor(lat[1:-1, 1:-1])
-    plt.xlabel('Does this work?')
-    plt.savefig(os.path.join('Figures/Step_100000_1.5Tc.eps'))
+        lat, __ = ising.ising_step(lat, size, t1)
+    ax6.pcolor(lat[1:-1, 1:-1])
+
+    lat = np.copy(base_lattice)
+    ax7.pcolor(lat[1:-1, 1:-1])
+
+    for __ in range(10000):
+        lat, __ = ising.ising_step(lat, size, t2)
+    ax8.pcolor(lat[1:-1, 1:-1])
+
+    for __ in range(90000):
+        lat, __ = ising.ising_step(lat, size, t2)
+    ax9.pcolor(lat[1:-1, 1:-1])
+
+    plt.show()
+
+
+def make_pcolor_long_fig():
+    f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex='col', sharey='row')
+    size = 100
+    tc = 1.1346
+    t0 = 0.25 * tc
+    base_lattice = 2 * np.random.randint(0, 2, size=(size+2, size+2)) - 1
+    base_lattice[ 0,  1:-1] = base_lattice[-2,  1:-1]
+    base_lattice[-1,  1:-1] = base_lattice[ 1,  1:-1]
+    base_lattice[ 1:-1,  0] = base_lattice[ 1:-1, -2]
+    base_lattice[ 1:-1, -1] = base_lattice[ 1:-1,  1]
+    base_lattice[0, 0] = base_lattice[-1, 0] = base_lattice[0, -1] = base_lattice[-1, -1] = 0
+    ax1.set_title("t=1")
+    ax2.set_title("t=10000")
+    ax3.set_title("t=100000")
+    ax4.set_title("t=1000000")
+    plt.suptitle('Time steps of MCMC simulation ($T=0.25T_c$)')
+    lat = np.copy(base_lattice)
+    ax1.pcolor(lat[1:-1,1:-1])
+    for __ in range(10000):
+        lat, __ = ising.ising_step(lat, size, t0)
+    ax2.pcolor(lat[1:-1,1:-1])
+    for __ in range(90000):
+        lat, __ = ising.ising_step(lat, size, t0)
+    ax3.pcolor(lat[1:-1,1:-1])
+    for __ in range(900000):
+        lat, __ = ising.ising_step(lat, size, t0)
+    ax4.pcolor(lat[1:-1,1:-1])
+    plt.show()
+
 
 
 def make_acvf_figures():
@@ -134,13 +137,13 @@ def make_acvf_figures():
         lat[ 1:-1,  0] = lat[ 1:-1, -2]
         lat[ 1:-1, -1] = lat[ 1:-1,  1]
         lat[0, 0] = lat[-1, 0] = lat[0, -1] = lat[-1, -1] = 0
-        en_arr[i, :], __ = ising_sw.ising_simulation(size, tc, nsteps, lat)
+        en_arr[i, :], __ = ising.ising_simulation(size, tc, nsteps, lat)
         print('FINISHED STEP {}'.format(i))
 
     acvf = []
     for i in range(nsims):
         print('CALC #{}'.format(i))
-        acvf.append(ising_sw.compute_ACcF(en_arr[i, :], 10000, 15000, 5000))
+        acvf.append(ising.compute_ACcF(en_arr[i, :], 10000, 15000, 5000))
 
     acvf = np.array(acvf)
     av = []
@@ -153,7 +156,54 @@ def make_acvf_figures():
     plt.ylabel("Autocorrelation")
     plt.savefig('Figures/acvf.eps')
 
+
+def make_energy_magnet_figures():
+    f, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.set_title('Energy vs Temperature')
+    ax2.set_title('Magnetization vs Temperature')
+    ax1.set_xlabel('Temperature')
+    ax1.set_ylabel('Energy')
+    ax2.set_xlabel('Temperature')
+    ax2.set_ylabel('Magnetization')
+    size = 16
+    niterations = 1000 * size**2 # Sweep over everything 2000 times
+    warmup = 100 # Ignore the first 100 sweeps
+    temp_crit = 1.1346
+    ntemps = 256
+    sample_temperatures = np.random.normal(temp_crit, 0.64, size=ntemps)
+    sample_temperatures = sample_temperatures[np.where(sample_temperatures>0)]
+    ntemps = len(sample_temperatures)
+    ener_array = np.zeros(ntemps)
+    magn_array = np.zeros(ntemps)
+
+    for ii, temp in enumerate(sample_temperatures):
+        print('Temperature {} of {}'.format(ii, ntemps))
+        lat = 2 * np.random.randint(0, 2, size=(size+2, size+2)) - 1
+        ehist = np.zeros(niterations+1)
+        mhist = np.zeros(niterations+1)
+        lat[ 0,  1:-1] = lat[-2,  1:-1]
+        lat[-1,  1:-1] = lat[ 1,  1:-1]
+        lat[ 1:-1,  0] = lat[ 1:-1, -2]
+        lat[ 1:-1, -1] = lat[ 1:-1,  1]
+        lat[0, 0] = lat[-1, 0] = lat[0, -1] = lat[-1, -1] = 0
+
+        ehist, mhist = ising.ising_simulation(size, temp, niterations, lat)
+        ehist /= size ** 2
+        ener_array[ii]=(ehist[warmup:].mean())
+        magn_array[ii]=(mhist[warmup:].mean())
+
+    ax1.scatter(sample_temperatures, ener_array)
+    ax2.scatter(sample_temperatures, magn_array)
+    plt.show()
+
+
+
+
+
+
 if __name__ == '__main__':
     #make_pcolor_figures()
-    make_warmup_time_figure()
+    #make_warmup_time_figure()
     #make_acvf_figures()
+    #make_energy_magnet_figures()
+    make_pcolor_long_fig()
